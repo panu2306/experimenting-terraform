476080b66a51a9c0ea40932f2421b0f63ab949c6
panu2306 <pranavbhendawade@gmail.com>
2024-03-30 15:12:30 -0700

    cross resource referencing - completed

---
+++ course/cross_resource_referencing.tf
@@ -0,0 +1,15 @@
+resource "aws_eip" "my_eip" {
+	domain 	= "vpc"
+}
+
+resource "aws_security_group" "allow_http_traffic" {
+	name	= "attribute-sg"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ingress_traffic" {
+	security_group_id 	= aws_security_group.allow_http.id
+	cidr_ipv4 		= "${aws_eip.my_eip.public_ip}/32"
+	from_port 		= 80
+	ip_protocol 		= "tcp"
+	to_port 		= 80
+}
