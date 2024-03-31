b8c09975115f463a4e95a882e771636de594939b
panu2306 <pranavbhendawade@gmail.com>
2024-03-31 12:50:15 -0700

    local values in terraform - completed

--- course/create_ec2.tf
+++ course/create_ec2.tf
@@ -2,13 +2,18 @@ provider "aws" {
 	region     = "us-west-1"
 }
 
+locals {
+	common_tags = {
+		owner 	= "DevOps Team"
+		service = "Backend Server"
+	}
+}
+
 resource "aws_instance" "my_first_ec2" {
 	ami 		= "ami-05c969369880fa2c2"
 	instance_type   = "t2.micro"
 	
-	tags = {
-		Name = "EC2-01"
-	}
+	tags = local.common_tags
 }
 
 output "ec2_info" {
