e28c8118474fdd1c8bc438ab78885d4cb59e5769
panu2306 <pranavbhendawade@gmail.com>
2024-03-30 21:21:11 -0700

    ouput values completed

--- course/create_ec2.tf
+++ course/create_ec2.tf
@@ -10,3 +10,11 @@ resource "aws_instance" "my_first_ec2" {
 		Name = "EC2-01"
 	}
 }
+
+output "ec2_info" {
+	value = aws_instance.my_first_ec2
+}
+
+output "ec2_ip_dns" {
+	value = "DNS: ${aws_instance.my_first_ec2.public_dns} and IP: ${aws_instance.my_first_ec2.public_ip}"
+}
