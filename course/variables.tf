9b0d5bd770fe1b3c3224a14e3c40b1712fe280a0
panu2306 <pranavbhendawade@gmail.com>
2024-03-30 22:26:31 -0700

    ways to define values for variables in terraform - completed

--- course/variables.tf
+++ course/variables.tf
@@ -1,15 +1,16 @@
 variable "cidr_block" {
-	default = "0.0.0.0/0"
+	description 	= "This is the definition of cidr block" 
 }
 
 variable "http_port" {
-	default = "80"
+	description 	= "This port is opened for web access" 
 }
 
 variable "ssh_port" {
-	default = "22"
+	description 	= "This port is defined for ssh access" 
 }
 
 variable "random_port" {
-	default = "8080"
+	default 	= "8080"
+	description 	= "This is the definition for variable random_port" 
 }
