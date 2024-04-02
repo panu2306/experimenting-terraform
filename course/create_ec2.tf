2cb8161a207eea7801ce4197dab7150e9efdfbb9
panu2306 <pranavbhendawade@gmail.com>
2024-04-01 17:04:31 -0700

    formatted the code using - terraform fmt command

--- course/create_ec2.tf
+++ course/create_ec2.tf
@@ -1,28 +1,42 @@
 provider "aws" {
-	region     = "us-west-1"
+  region = "us-west-1"
 }
 
 locals {
-	common_tags = {
-		owner 	= "DevOps Team"
-		service = "Backend Server"
-	}
-	time = formatdate("EEEE, DD-MMM-YY hh:mm:ss ZZZ", timestamp())
+  common_tags = {
+    owner   = "DevOps Team"
+    service = "Backend Server"
+  }
+  time = formatdate("EEEE, DD-MMM-YY hh:mm:ss ZZZ", timestamp())
+}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20240301"]
+  }
 }
 
 resource "aws_instance" "my_ec2" {
-	ami 		= "${lookup(var.ami_list, var.regions[2])}"
-	instance_type   = "t2.micro"
-	
-	count = 2
+  ami           = lookup(var.ami_list, var.regions[2])
+  instance_type = "t2.micro"
 
-	tags = "${merge(
-		local.common_tags, 
-		{"Name" = "EC2-${count.index}"}
-	)}"
+  count = 2
+
+  tags = (merge(
+    local.common_tags,
+    { "Name" = "EC2-${count.index}" }
+  ))
 }
 
+resource "aws_instance" "data_source_ec2" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t2.micro"
+}
 
 output "time_created" {
-	value = local.time
+  value = local.time
 }
--- course/variables.tf
+++ course/variables.tf
@@ -1,30 +1,30 @@
 variable "cidr_block" {
-	description 	= "This is the definition of cidr block" 
+  description = "This is the definition of cidr block"
 }
 
 variable "http_port" {
-	description 	= "This port is opened for web access" 
+  description = "This port is opened for web access"
 }
 
 variable "ssh_port" {
-	description 	= "This port is defined for ssh access" 
+  description = "This port is defined for ssh access"
 }
 
 variable "random_port" {
-	default 	= "8080"
-	description 	= "This is the definition for variable random_port" 
+  default     = "8080"
+  description = "This is the definition for variable random_port"
 }
 
 variable "ami_list" {
-	type = map 
-	default = {
-		"us-east-1"	= "ami-080e1f13689e07408"
-		"us-east-2"  	= "ami-0b8b44ec9a8f90422"
-		"us-west-1"  	= "ami-05c969369880fa2c2"
-		"us-west-2"  	= "ami-08116b9957a259459"
-	}
+  type = map(any)
+  default = {
+    "us-east-1" = "ami-080e1f13689e07408"
+    "us-east-2" = "ami-0b8b44ec9a8f90422"
+    "us-west-1" = "ami-05c969369880fa2c2"
+    "us-west-2" = "ami-08116b9957a259459"
+  }
 }
 
 variable "regions" {
-	type = list(string)
+  type = list(string)
 }
