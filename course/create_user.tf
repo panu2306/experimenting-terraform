bd26b0a0fd1c5062e79cf3faac1bb13dd8820fa9
panu2306 <pranavbhendawade@gmail.com>
2024-04-01 18:35:06 -0700

    zipmap function - completed

--- course/create_user.tf
+++ course/create_user.tf
@@ -1,10 +1,17 @@
 resource "aws_iam_user" "lb" {
-	name = "iamUser.${count.index}"
-	count = 3
-	path = "/system/"
+  name  = "iamUser.${count.index}"
+  count = 3
+  path  = "/system/"
 }
 
 output "arns" {
-	value = aws_iam_user.lb[*].arn
+  value = aws_iam_user.lb[*].arn
 }
 
+output "names" {
+  value = aws_iam_user.lb[*].name
+}
+
+output "combined" {
+  value = zipmap(aws_iam_user.lb[*].name, aws_iam_user.lb[*].arn)
+}
